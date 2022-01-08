5a57f3f1533da339c8f3fe3247b31634f63b7f30
Zedkindevil <94899615+Zedkindevil@users.noreply.github.com>
2022-01-08 16:02:28 +0530

    Update config.py

--- AstrakoBot/config.py
+++ AstrakoBot/config.py
@@ -41,13 +41,13 @@ class Config(object):
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    SUDO_USERS = get_user_list("elevated_users.json", "5010513283")
+    SUDO_USERS = get_user_list("elevated_users.json", "sudos")
     ##List of id's - (not usernames) for developers who will have the same perms as the owner
-    DEV_USERS = get_user_list("elevated_users.json", "5010513283")
+    DEV_USERS = get_user_list("elevated_users.json", "devs")
     ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    SUPPORT_USERS = get_user_list("elevated_users.json", "5010513283")
+    SUPPORT_USERS = get_user_list("elevated_users.json", "supports")
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    WHITELIST_USERS = get_user_list("elevated_users.json", "5010513283")
+    WHITELIST_USERS = get_user_list("elevated_users.json", "whitelists")
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
     PORT = 5000
